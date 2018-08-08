ae9d412e832b16e6a363b8f203e221be101f7264
zhenglibin <zhenglibin@tuandai.com>
2018-08-08 13:59:06 +0800

    修改

--- src/main/java/com/example/test/test.java
+++ src/main/java/com/example/test/test.java
@@ -3,13 +3,9 @@ package com.example.test;
 public class test {
 
 	public static void main(String[] args) {
-		try {
-			String s = null;
-			s.toString();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-
+		String nickName = "你好啊";
+		nickName = (nickName != null && nickName.length() > 3) ? "****" + nickName.substring(nickName.length()-3): nickName ;
+		System.out.println(nickName);
 	}
 
 }
