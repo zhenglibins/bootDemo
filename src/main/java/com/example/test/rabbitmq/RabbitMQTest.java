12913bfd82e22ed27ff0a6db4d7cb5633f3cee6c
zhenglibin <zhenglibin@tuandai.com>
2019-01-29 18:26:42 +0800

    ceshi

--- src/main/java/com/example/test/rabbitmq/RabbitMQTest.java
+++ src/main/java/com/example/test/rabbitmq/RabbitMQTest.java
@@ -10,17 +10,19 @@ import java.io.IOException;
 public class RabbitMQTest {
     public static final String QUEUE_NAME = "MYFIRSTQUENE";
     public static void main(String args[])throws Exception{
-       sendMSG("hello,zhenglibin");
-//        Thread.sleep(10000);
-//        recieveMSG();
+
+        sendMSG("hello,zhenglibin");
+        Thread.sleep(10000);
+        recieveMSG();
+
     }
     public static void sendMSG(String msg) throws Exception{
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        factory.setHost("47.106.214.38");
         Connection connection = factory.newConnection();
         //创建一个通道
         Channel channel = connection.createChannel();
-        //channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         String message = msg;
         //发送消息到队列中
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
@@ -34,13 +36,13 @@ public class RabbitMQTest {
         // 创建连接工厂
         ConnectionFactory factory = new ConnectionFactory();
         //设置RabbitMQ地址
-        factory.setHost("localhost");
+        factory.setHost("47.106.214.38");
         //创建一个新的连接
         Connection connection = factory.newConnection();
         //创建一个通道
         Channel channel = connection.createChannel();
         //声明要关注的队列
-        channel.queueDeclare(QUEUE_NAME, false, false, true, null);
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         System.out.println("Customer Waiting Received messages");
         //DefaultConsumer类实现了Consumer接口，通过传入一个频道，
         // 告诉服务器我们需要那个频道的消息，如果频道中有消息，就会执行回调函数handleDelivery
@@ -51,9 +53,10 @@ public class RabbitMQTest {
                     throws IOException {
                 String message = new String(body, "UTF-8");
                 System.out.println("Customer Received '" + message + "'");
+                channel.basicReject(envelope.getDeliveryTag(),false);
             }
         };
         //自动回复队列应答 -- RabbitMQ中的消息确认机制
-        channel.basicConsume(QUEUE_NAME, true, consumer);
+        channel.basicConsume(QUEUE_NAME, false, consumer);
     }
 }
